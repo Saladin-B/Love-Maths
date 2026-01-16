2f8523548398cb9418546b41b8f0930e47618135
Saladin-B <saladeen.baalache@gmail.com>
2026-01-16 21:53:53 +0000

    fixed variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function runGame() {
     // Generate two random numbers between 1 and 25
-    let num 1 = Math.floor(Math.random() * 25) + 1;
-    let num 2 = Math.floor(Math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {

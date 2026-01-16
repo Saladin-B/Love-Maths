8f4fae0abf02b3397d441f982daaea5bbcbc46ad
Saladin-B <saladeen.baalache@gmail.com>
2026-01-16 22:15:28 +0000

    display addition to question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,14 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
+
     }
+    
+     runGame("addition");
+
 });
 
 /**
@@ -18,10 +23,17 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     // Generate two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -38,6 +50,12 @@ function incrementScore() {
 
 function incrementWrongAnswer() {
     
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").innerText = operand1;
+    document.getElementById("operand2").innerText = operand2;
+    document.getElementById("operator").innerText = "+";
 }
 
 function displaySubtractQuestion() {
